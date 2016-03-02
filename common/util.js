1c6f3e3aa94245d067afd3f758c6fa4b0c27d5ee
Dmytro V. Dogadailo <dvd@dogada.org>
2016-03-02 13:34:00 +0200

    Added coect.util.truncate

--- common/util.js
+++ common/util.js
@@ -1,3 +1,5 @@
+'use strict';
+
 exports.truncateUrl = function (url, limit) {
   limit = limit || 30
   url = url.replace(/http[s]?:\/\/(www\.)?/i, '')
@@ -5,3 +7,16 @@ exports.truncateUrl = function (url, limit) {
   var segment = Math.floor(limit / 2) - 1
   return url.slice(0, segment) + '..' + url.slice(url.length - segment) 
 }
+
+exports.truncate = function(str, maxLen, minLen) {
+  var res = str.trim().slice(0, maxLen), lastSpace = res.lastIndexOf(' ')
+  for (let sep of ['.', '?', '!', ';', ',', ' ']) {
+    let i = res.lastIndexOf(sep)
+    if (i > (minLen || maxLen/3)) {
+      res = res.slice(0, i)
+      break
+    }
+  }
+  return res.trim()
+}
+
