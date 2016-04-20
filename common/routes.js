1bd5f63217b4d643d48726ef84ac1e48f0186f45
Dmytro V. Dogadailo <dvd@dogada.org>
2016-04-20 12:29:12 +0300

    Added routes.parseQuery.

--- common/routes.js
+++ common/routes.js
@@ -2,3 +2,12 @@
 exports.slug = function(param) {
   return '/:' + param + '([a-z]+[a-z0-9-]{2,})'
 }
+
+exports.parseQuery = function (query) {
+  var search = /([^&=]+)=?([^&]*)/g,
+  decode = (s) => decodeURIComponent(s.replace (/\+/g, ' ')),
+  res = {}, match
+
+  while ((match = search.exec(query))) res[decode(match[1])] = decode (match[2])
+  return res
+}
